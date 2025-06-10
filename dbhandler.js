2e3e7dd1ca79f3517fbe4d7971f1a558ca67c706
toth-marcell <toth.marcell.21i@egressy.info>
2025-06-10 08:40:09 +0200

    import `uj`

---
+++ dbhandler.js
@@ -0,0 +1,78 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const handler = new Sequelize('data', 'root', '', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+
+exports.categories = handler.define('category', {  
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  }
+}, {
+  tableName: 'categorytable'
+});
+
+
+exports.recipes = handler.define('recipe', { 
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  ingredients: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'categorytable',
+      key: 'id'
+    }
+  }
+}, {
+  tableName: 'recipetable'
+});
+
+exports.users = handler.define('usertable', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+
+exports.categories.hasMany(exports.recipes, { foreignKey: 'categoryId' });
+exports.recipes.belongsTo(exports.categories, { 
+  foreignKey: 'categoryId',
+  targetKey: 'id',
+  onDelete: 'SET NULL',
+  onUpdate: 'CASCADE'
+});
+
